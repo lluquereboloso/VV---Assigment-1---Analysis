94f4e998dc7ccd77bac4377e5a8eee247a9122c1
Rafa <r.martinc@outlook.com>
2017-11-27 22:35:11 +0100

    Añadidas pruebas de Agenda2.numeroPersonas()

--- DinamicAnalysis/src/test/java/agendalista/Agenda2Test.java
+++ DinamicAnalysis/src/test/java/agendalista/Agenda2Test.java
@@ -8,11 +8,22 @@ import junit.framework.TestCase;
 
 public class Agenda2Test extends TestCase {
 
-	Agenda2 agenda; 
+	Agenda2 agenda;
+	Persona p1;
 	
 	@Before
 	protected void setUp() throws Exception {
 		agenda = new Agenda2();
+		p1 = new Persona();
+
+		p1.ponerNombre("Luis");
+		p1.ponerApellidos("Luque");
+		p1.ponerAnioNacim(1993);
+		p1.ponerCodPostal("28032");
+		p1.ponerDireccion("calle");
+		p1.ponerPoblacion("Madrid");
+		p1.ponerProvincia("Madrid");
+		p1.ponerTelefono("91");
 	}
 
 	@After
@@ -27,43 +38,16 @@ public class Agenda2Test extends TestCase {
 
 	@Test
 	public void testAniadirPersona() {
-		Persona p1 = new Persona();
-		p1.ponerNombre("Luis");
-		p1.ponerApellidos("Luque");
-		p1.ponerAnioNacim(1993);
-		p1.ponerCodPostal("28032");
-		p1.ponerDireccion("calle");
-		p1.ponerPoblacion("Madrid");
-		p1.ponerProvincia("Madrid");
-		p1.ponerTelefono("91");
 		assertTrue(agenda.aniadirPersona(p1));
 	}
 
 	@Test
 	public void testEliminarPersona() {
-		Persona p1 = new Persona();
-		p1.ponerNombre("Luis");
-		p1.ponerApellidos("Luque");
-		p1.ponerAnioNacim(1993);
-		p1.ponerCodPostal("28032");
-		p1.ponerDireccion("calle");
-		p1.ponerPoblacion("Madrid");
-		p1.ponerProvincia("Madrid");
-		p1.ponerTelefono("91");
 		agenda.aniadirPersona(p1);
 		assertTrue(agenda.eliminarPersona(p1.obtenerNombreCompleto()));
 	}
 
 	public void testQuitarPrimero() {
-		Persona p1 = new Persona();
-		p1.ponerNombre("Luis");
-		p1.ponerApellidos("Luque");
-		p1.ponerAnioNacim(1993);
-		p1.ponerCodPostal("28032");
-		p1.ponerDireccion("calle");
-		p1.ponerPoblacion("Madrid");
-		p1.ponerProvincia("Madrid");
-		p1.ponerTelefono("91");
 		agenda.aniadirPersona(p1);
 		assertTrue( p1 == agenda.quitarPrimero());
 	}
@@ -72,7 +56,13 @@ public class Agenda2Test extends TestCase {
 	}
 
 	public void testNumeroPersonas() {
-
+		
+		// Test case 1
+		assertTrue(agenda.numeroPersonas() == 0);
+		
+		// Test case 2
+		agenda.aniadirPersona(p1);
+		assertTrue(agenda.numeroPersonas() == 1);
 	}
 
 	public void testGuardarAgenda() {
@@ -82,5 +72,4 @@ public class Agenda2Test extends TestCase {
 	public void testRecuperarAgenda() {
 
 	}
-
 }
