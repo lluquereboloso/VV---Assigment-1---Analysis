fa92fc65010098fd9c828a50d29438fa8c187995
Unknown <r.almaraz@alumnos.upm.es>
2017-11-27 12:12:31 +0100

    Agregado Tests para metodos aniadir y eliminar de clase Agenda2

---
+++ DinamicAnalysis/src/test/java/agendalista/Agenda2Test.java
@@ -0,0 +1,78 @@
+package agendalista;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Agenda2Test extends TestCase {
+
+	Agenda2 agenda; 
+	
+	@Before
+	protected void setUp() throws Exception {
+		agenda = new Agenda2();
+	}
+
+	@After
+	protected void tearDown() throws Exception {
+		for(int i=0; i<agenda.numeroPersonas(); i++){
+			agenda.quitarPrimero();
+		}
+	}
+
+	public void testAgenda2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAniadirPersona() {
+		Persona p1 = new Persona();
+		p1.ponerNombre("Luis");
+		p1.ponerApellidos("Luque");
+		p1.ponerAnioNacim(1993);
+		p1.ponerCodPostal("28032");
+		p1.ponerDireccion("calle");
+		p1.ponerPoblacion("Madrid");
+		p1.ponerProvincia("Madrid");
+		p1.ponerTelefono("91");
+		assertTrue(agenda.aniadirPersona(p1));
+	}
+
+	@Test
+	public void testEliminarPersona() {
+		Persona p1 = new Persona();
+		p1.ponerNombre("Luis");
+		p1.ponerApellidos("Luque");
+		p1.ponerAnioNacim(1993);
+		p1.ponerCodPostal("28032");
+		p1.ponerDireccion("calle");
+		p1.ponerPoblacion("Madrid");
+		p1.ponerProvincia("Madrid");
+		p1.ponerTelefono("91");
+		agenda.aniadirPersona(p1);
+		assertTrue(agenda.eliminarPersona(p1.obtenerNombreCompleto()));
+	}
+
+	public void testQuitarPrimero() {
+		fail("Not yet implemented");
+	}
+
+	public void testEstaVacia() {
+		fail("Not yet implemented");
+	}
+
+	public void testNumeroPersonas() {
+		fail("Not yet implemented");
+	}
+
+	public void testGuardarAgenda() {
+		fail("Not yet implemented");
+	}
+
+	public void testRecuperarAgenda() {
+		fail("Not yet implemented");
+	}
+
+}
